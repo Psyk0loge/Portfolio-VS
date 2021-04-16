c8ef7527f86576d43ef2d7c730683e037b935ca2
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-04-16 14:45:34 +0200

    sick

--- src/Model_Single_Threaded/Server.java
+++ src/Model_Single_Threaded/Server.java
@@ -6,11 +6,14 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server extends Thread {
 
 	private static final int DEFAULT_PORT = 7777;
-	
-	public static void main(String[] args) {
+	private static final int MAX = 15;
+	private static final int MIN = 2;
+
+	@Override
+	public void run() {
 		BufferedReader clientIn;
 
 		Socket conn = null;
@@ -24,8 +27,11 @@ public class Server {
 					clientIn = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 					String userID = clientIn.readLine();
 					System.out.println("Clientnachricht: " + userID);
+					sleep((long) (Math.floor(Math.random() * (MAX - MIN + 1) + MIN) * 1000));
 
 				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} finally {
 					try {
