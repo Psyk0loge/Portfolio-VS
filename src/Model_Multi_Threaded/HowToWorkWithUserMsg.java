89226a29cfcfa15c060c21b9bf4c8ec94a74a22d
Manolo Monetha <Manolo.Monetha@sobis.com>
2021-05-07 17:55:21 +0200

    fsa

--- src/Model_Multi_Threaded/HowToWorkWithUserMsg.java
+++
@@ -1,5 +0,0 @@
-package Model_Multi_Threaded;
-
-public enum HowToWorkWithUserMsg {
-	GET, ADD
-}
--- src/Model_Multi_Threaded/HowToWorkWithWorkerThread.java
+++
@@ -1,5 +0,0 @@
-package Model_Multi_Threaded;
-
-public enum HowToWorkWithWorkerThread {
-	IS_EMPTY, ADD, REMOVE
-}
